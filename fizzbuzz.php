e46a1d53fa03c701774bdbabd68e33e6f6bc0def
Justin <Justin@Justins-MacBook-Pro.local>
2014-05-13 13:09:24 -0500

    Created a list from 1 to 100 with fizz and buzz words used in numbers with different multiples

---
+++ fizzbuzz.php
@@ -0,0 +1,36 @@
+<?php
+
+//pring numbers between 1 and 100
+//if divisible by 3 write fizz
+//if divisible by 5 write buzz
+//if divisible by 3 and 5 write fizzbuzz
+
+//for ($a = 1; $a <= 100; $a++) {echo "{$a}\n"; 
+
+//	if ($a % 3 == 0) {echo "fizz\n";}	
+
+for ($i = 1; $i <= 100  ; $i++) {
+
+	if ($i % 15 == 0) {
+
+		echo "buzz\n";
+	}
+
+	elseif ($i % 3 == 0) {
+
+		echo "fizz\n";
+	}
+
+
+	elseif ($i % 5 == 0) {
+
+		echo "buzz\n";
+	}
+
+	else  {
+		echo $i . PHP_EOL;
+	}
+}
+
+
+
