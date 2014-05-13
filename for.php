4c0321928baaf80ef240ba79f111f3039f52189c
Justin <Justin@Justins-MacBook-Pro.local>
2014-05-13 13:05:28 -0500

    Created prompts for a number range and listed all numbers witihin using and incrementer

---
+++ for.php
@@ -0,0 +1,28 @@
+<?php
+
+fwrite(STDOUT, "Please choose an increment\n");
+
+$increment = trim(fgets(STDIN));
+
+fwrite(STDOUT, "Please choose a number\n");
+
+$first_num = trim(fgets(STDIN));
+
+if (!is_numeric($first_num)) {echo "Please enter a numeric value\n";}
+
+if ($first_num = "") {echo "Please enter a numeric value\n";}
+
+fwrite(STDOUT, "Please choose another number\n");
+
+$second_num = trim(fgets(STDIN));
+
+if (!is_numeric($second_num)) {echo "Please enter a numeric value";}	
+
+if ($second_num = "") {echo "Please enter a numeric value";}	
+
+for ($i = $first_num; $i <= $second_num; $i = $increment++) {
+	echo "{$i}\n";
+}
+
+
+
