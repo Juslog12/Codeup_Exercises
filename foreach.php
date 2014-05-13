4fdaf90351a7cda22e748ecca2b2c0de6ac5805c
Justin <Justin@Justins-MacBook-Pro.local>
2014-05-13 14:54:05 -0500

    constructed to show only values with type scalar

--- foreach.php
+++ foreach.php
@@ -2,7 +2,11 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 foreach ($things as $thing) {
-	if (is_integer($thing)) {
+	if (is_scalar($thing)) {
+		echo "{$thing}\n";
+	} elseif (!is_scalar($thing)) {
+		echo "\n";
+	} elseif (is_integer($thing)) {
 		echo "{$thing} is a integer\n";
 	} elseif (is_float($thing)) {
 		echo "{$thing} is a float\n";
