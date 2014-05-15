56871614c38594c0989f4f3d921d67d6676d04d1
Justin <Justin@Justins-MacBook-Pro.local>
2014-05-15 14:16:25 -0500

    Refractored the error messages into their own function

--- arithmetic.php
+++ arithmetic.php
@@ -1,10 +1,18 @@
 <?php
 
+function error($a, $b, $flag){
+	if ($flag === 1) {
+		echo "ERROR: both {$a} and {$b} should be numbers\n";
+	} else {
+		echo "Cannot divide by zero. Please choose another number\n";
+	}
+}
+
 function add($a, $b) {
 	if (is_numeric ($a) && is_numeric($b)) {
     	echo $a + $b .PHP_EOL;
     } else {
-		echo "ERROR: both {$a} and {$b} should be numbers\n";
+		error($a, $b, 1);
 	}	
 }
 
@@ -12,7 +20,7 @@ function subtract($a, $b) {
     if (is_numeric ($a) && is_numeric ($b)) {
     	echo $a - $b .PHP_EOL;
     } else {
-		echo "ERROR: both {$a} and {$b} should be numbers\n";
+		error($a, $b, 1);
 	}
 }
 
@@ -20,17 +28,17 @@ function multiply($a, $b) {
    if (is_numeric ($a) && is_numeric($b)) { 
     	echo $a * $b .PHP_EOL;
     } else {
-		echo "ERROR: both {$a} and {$b} should be numbers\n";
+		error($a, $b, 1);
 	}
 }
 
 function divide($a, $b) {
     if  ($b == 0) {
-		echo "Cannot divide by zero.  Please choose another number\n";
+		error($a, $b, 2);
 	} elseif (is_numeric ($a) && is_numeric($b)) { 
     	echo $a / $b .PHP_EOL;
     } else {
-		echo "ERROR: both {$a} and {$b} should be numbers\n";
+		error($a, $b, 1);
 	}
 }
 
@@ -38,16 +46,12 @@ function modulus($a, $b) {
    if (is_numeric ($a) && is_numeric($b)) { 
     	echo $a % $b .PHP_EOL;
     } else {
-		echo "ERROR: both {$a} and {$b} should be numbers\n";
+		error($a, $b, 1);
 	}
 }
 
-add(20, J);
+add(20, "J");
 subtract(5, 10);
 multiply(3, 3);
 divide(2, 0);
-modulus (3,j);
-
-
-
-
+modulus (3,'J');
