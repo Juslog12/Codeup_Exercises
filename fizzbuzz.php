d1da825034e39b754e5c2107b8283d96e49c5daf
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:17:49 -0500

    Updated formatting indent spaces

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -4,26 +4,26 @@
 for ($i = 1; $i <= 100  ; $i++) 
 {
 //if divisible by 3 and 5 write fizzbuzz
-	if ($i % 15 == 0) 
-	{
-		echo "fizzbuzz\n";
-	}
+    if ($i % 15 == 0) 
+    {
+        echo "fizzbuzz\n";
+    }
 //if divisible by 3 write fizz
-//	if ($a % 3 == 0) {echo "fizz\n";}	
-	elseif ($i % 3 == 0) 
-	{
-		echo "fizz\n";
-	}
+//  if ($a % 3 == 0) {echo "fizz\n";}   
+    elseif ($i % 3 == 0) 
+    {
+        echo "fizz\n";
+    }
 //if divisible by 5 write buzz
-	elseif ($i % 5 == 0) 
-	{
-		echo "buzz\n";
-	}
+    elseif ($i % 5 == 0) 
+    {
+        echo "buzz\n";
+    }
 
-	else  
-	{
-		echo $i . PHP_EOL;
-	}
+    else  
+    {
+        echo $i . PHP_EOL;
+    }
 }
 
 
