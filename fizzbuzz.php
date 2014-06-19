66e63195213fbfc79dd7146dabdc44aa5f87d339
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:10:21 -0500

    Updated formatting.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,33 +1,27 @@
 <?php
-
-//pring numbers between 1 and 100
-//if divisible by 3 write fizz
-//if divisible by 5 write buzz
-//if divisible by 3 and 5 write fizzbuzz
-
+// numbers between 1 and 100
 //for ($a = 1; $a <= 100; $a++) {echo "{$a}\n"; 
-
-//	if ($a % 3 == 0) {echo "fizz\n";}	
-
-for ($i = 1; $i <= 100  ; $i++) {
-
-	if ($i % 15 == 0) {
-
+for ($i = 1; $i <= 100  ; $i++) 
+{
+//if divisible by 3 and 5 write fizzbuzz
+	if ($i % 15 == 0) 
+	{
 		echo "fizzbuzz\n";
 	}
-
-	elseif ($i % 3 == 0) {
-
+//if divisible by 3 write fizz
+//	if ($a % 3 == 0) {echo "fizz\n";}	
+	elseif ($i % 3 == 0) 
+	{
 		echo "fizz\n";
 	}
-
-
-	elseif ($i % 5 == 0) {
-
+//if divisible by 5 write buzz
+	elseif ($i % 5 == 0) 
+	{
 		echo "buzz\n";
 	}
 
-	else  {
+	else  
+	{
 		echo $i . PHP_EOL;
 	}
 }
