c817de19f9853dc2ec0a183a427778993fcbc336
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:27:25 -0500

    Updated formatting.

--- while.php
+++ while.php
@@ -1,11 +1,5 @@
 <?php
 
-//write output
-//fwrite(STDOUT, "Pick a number between 1-100. What is your number")
-//Get the input from user
-//$variable = fgets(STDIN)
-//$variable == (int) 2
-
 $min = 1;
 $max = 100;
 
