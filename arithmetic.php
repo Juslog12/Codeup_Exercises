d89c56c3f54d7b0015cf7366071c9965ec41767e
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:04:47 -0500

    Updated formatting.

--- arithmetic.php
+++ arithmetic.php
@@ -1,51 +1,75 @@
 <?php
 
-function error($a, $b, $flag){
-	if ($flag === 1) {
-		echo "ERROR: both {$a} and {$b} should be numbers\n";
-	} else {
-		echo "Cannot divide by zero. Please choose another number\n";
-	}
+function error($a, $b, $flag)
+{
+    if ($flag === 1) 
+    {
+        echo "ERROR: both {$a} and {$b} should be numbers\n";
+    } 
+    else 
+    {
+        echo "Cannot divide by zero. Please choose another number\n";
+    }
 }
 
-function add($a, $b) {
-	if (is_numeric ($a) && is_numeric($b)) {
-    	return $a + $b .PHP_EOL;
-    } else {
-		error($a, $b, 1);
-	}	
+function add($a, $b) 
+{
+    if (is_numeric ($a) && is_numeric($b)) 
+    {
+        return $a + $b .PHP_EOL;
+    } 
+    else 
+    {
+        error($a, $b, 1);
+    }   
 }
 
-function subtract($a, $b) {
-    if (is_numeric ($a) && is_numeric ($b)) {
-    	return $a - $b .PHP_EOL;
-    } else {
-		error($a, $b, 1);
-	}
+function subtract($a, $b) 
+{
+    if (is_numeric ($a) && is_numeric ($b)) 
+    {
+        return $a - $b .PHP_EOL;
+    } 
+    else 
+    {
+        error($a, $b, 1);
+    }
 }
 
-function multiply($a, $b) {
-   if (is_numeric ($a) && is_numeric($b)) { 
-    	return $a * $b .PHP_EOL;
-    } else {
-		error($a, $b, 1);
-	}
+function multiply($a, $b) 
+{
+   if (is_numeric ($a) && is_numeric($b)) 
+   { 
+        return $a * $b .PHP_EOL;
+    } 
+    else 
+    {
+        error($a, $b, 1);
+    }
 }
 
-function divide($a, $b) {
-    if ($b == 0) {
-    	return "Cannot divide by ";
-	} elseif (is_numeric($a) && is_numeric($b)) {
-    	return $a / $b .PHP_EOL;
-    }	 
+function divide($a, $b) 
+{
+    if ($b == 0) 
+    {
+        return "Cannot divide by ";
+    } 
+    elseif (is_numeric($a) && is_numeric($b)) 
+    {
+        return $a / $b .PHP_EOL;
+    }    
 }
 
-function modulus($a, $b) {
-   if (is_numeric ($a) && is_numeric($b)) { 
-    	return $a % $b .PHP_EOL;
-    } else {
-		error($a, $b, 1);
-	}	
+function modulus($a, $b) 
+{
+   if (is_numeric ($a) && is_numeric($b)) 
+   { 
+        return $a % $b .PHP_EOL;
+    } 
+    else 
+    {
+        error($a, $b, 1);
+    }   
 }
 
 echo add(20, 5);
@@ -53,3 +77,5 @@ echo subtract(5, 10);
 echo multiply(3, 3);
 echo divide(2, 0);
 echo modulus (3,1);
+
+?>
