e217d3cc5fd64011c34aa0ef24a5bf5b9107d417
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:17:04 -0500

    Update formatting.

--- foreach.php
+++ foreach.php
@@ -2,40 +2,40 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-	foreach ($things as $thing) 
-	{
-		if (is_scalar($thing)) 
-		{
-			echo "{$thing}\n";
-		} 
-		elseif (!is_scalar($thing)) 
-		{
-			echo "\n";
-		} 
-		elseif (is_integer($thing)) 
-		{
-			echo "{$thing} is a integer\n";
-		} 
-		elseif (is_float($thing)) 
-		{
-		echo "{$thing} is a float\n";
-		} 
-		elseif (is_bool($thing)) 
-		{
-			echo "{$thing} is a boolean\n";
-		} 
-		elseif (is_array($thing)); 
-		{
-			echo "{$thing} is a array\n";
-		} 
-		elseif (is_null($thing)) 
-		{
-			echo "{$thing} is null\n";
-		} 
-		elseif (is_string($thing)) 
-		{
-		echo "{$thing} is string\n";
-		}
-	}
+    foreach ($things as $thing) 
+    {
+        if (is_scalar($thing)) 
+        {
+            echo "{$thing}\n";
+        } 
+        elseif (!is_scalar($thing)) 
+        {
+            echo "\n";
+        } 
+        elseif (is_integer($thing)) 
+        {
+            echo "{$thing} is a integer\n";
+        } 
+        elseif (is_float($thing)) 
+        {
+        echo "{$thing} is a float\n";
+        } 
+        elseif (is_bool($thing)) 
+        {
+            echo "{$thing} is a boolean\n";
+        } 
+        elseif (is_array($thing)); 
+        {
+            echo "{$thing} is a array\n";
+        } 
+        elseif (is_null($thing)) 
+        {
+            echo "{$thing} is null\n";
+        } 
+        elseif (is_string($thing)) 
+        {
+        echo "{$thing} is string\n";
+        }
+    }
 
 ?>
