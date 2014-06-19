5d202a4d6837b6148969480a88d587f3dd068c82
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:18:38 -0500

    convert to indentation spaces

--- for.php
+++ for.php
@@ -2,20 +2,20 @@
 
 fwrite(STDOUT, "Please choose an increment\n");
 
-	$increment = trim(fgets(STDIN));
+    $increment = trim(fgets(STDIN));
 
 fwrite(STDOUT, "Please choose a number\n");
 
-	$first_num = trim(fgets(STDIN));
+    $first_num = trim(fgets(STDIN));
 
 if (!is_numeric($first_num)) 
 {
-	echo "Please enter a numeric value\n";
+    echo "Please enter a numeric value\n";
 }
 
 if ($first_num = "") 
 {
-		echo "Please enter a numeric value\n";
+        echo "Please enter a numeric value\n";
 }
 
 fwrite(STDOUT, "Please choose another number\n");
@@ -24,17 +24,17 @@ $second_num = trim(fgets(STDIN));
 
 if (!is_numeric($second_num)) 
 {
-	echo "Please enter a numeric value";
-}	
+    echo "Please enter a numeric value";
+}   
 
 if ($second_num = "") 
 {
-	echo "Please enter a numeric value";
-}	
+    echo "Please enter a numeric value";
+}   
 
 for ($i = $first_num; $i <= $second_num; $i = $increment++) 
 {
-	echo "{$i}\n";
+    echo "{$i}\n";
 }
 
 ?>
