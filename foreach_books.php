510f4a116dc712c924dfe0976226d2c09f58d2c0
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:20:37 -0500

    Updated formatting.

--- foreach_books.php
+++ foreach_books.php
@@ -1,6 +1,7 @@
 <?php
 
-$books = array(
+$books = array
+(
      array('name' => 'The Hobbit',
         'published' => 1937,
         'author' => 'J. R. R. Tolkien',
@@ -23,40 +24,50 @@ $books = array(
     )
 );
 
-foreach ($books as $book) {
-	if ($book['published'] > 1950) {
+foreach ($books as $book) 
+{
+    if ($book['published'] > 1950) {
     echo "{$book['name']}, published in {$book["published"]}, was written by {$book["author"]}, and has {$book["pages"]} pages.\n";
-	}
+    }
 }
 
 // instructors version
 
-$books = array(
-    'The Hobbit' => array(
+$books = array
+(
+    'The Hobbit' => array
+    (
         'published' => 1937,
         'author' => 'J. R. R. Tolkien',
         'pages' => 310
     ),
-    'Game of Thrones' => array(
+    'Game of Thrones' => array
+    (
         'published' => 1996,
         'author' => 'George R. R. Martin',
         'pages' => 835
     ),
-    'The Catcher in the Rye' => array(
+    'The Catcher in the Rye' => array
+    (
         'published' => 1951,
         'author' => 'J. D. Salinger',
         'pages' => 220
     ),
-    'A Tale of Two Cities' => array(
+    'A Tale of Two Cities' => array
+    (
         'published' => 1859,
         'author' => 'Charles Dickens',
         'pages' => 544
     )
 );
 
-foreach ($books as $book => $properties) {
-	echo $book  . PHP_EOL;
-	foreach ($properties as $property => $value) {
-		echo "{$property}: {$value}\n";
-	}
+foreach ($books as $book => $properties) 
+{
+    echo $book  . PHP_EOL;
+    foreach ($properties as $property => $value) 
+    {
+        echo "{$property}: {$value}\n";
+    }
 }
+
+?>
