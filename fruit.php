a6bb81cf01cb9a1c51013e9e85dafe76d161923e
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:21:43 -0500

    Updated formatting.

--- fruit.php
+++ fruit.php
@@ -1,33 +1,40 @@
 <?php
 
-$fruits = array (
-	array ('name' => 'apples', 'color' => 'red'),
-	array ('name' => 'oranges', 'color' => 'orange'),
-	array ('name' => 'bananas', 'color' => 'yellow'),
-	);
+$fruits = array 
+(
+    array ('name' => 'apples', 'color' => 'red'),
+    array ('name' => 'oranges', 'color' => 'orange'),
+    array ('name' => 'bananas', 'color' => 'yellow'),
+);
 
-foreach ($fruits as $fruit) {
-	echo "{$fruit['name']} are {$fruit['color']} and delicious.\n";
+foreach ($fruits as $fruit) 
+{
+    echo "{$fruit['name']} are {$fruit['color']} and delicious.\n";
 }
 
 
-$fruits = (
-	'apples' => 'red',
-	'oranges' => 'orange',
-	'bananas' => 'yellow'),
-	);
+$fruits = 
+(
+    'apples' => 'red',
+    'oranges' => 'orange',
+    'bananas' => 'yellow'),
+);
 
-foreach ($fruits as $fruit) {
-	echo "{$fruit} are {$color}\n";
+foreach ($fruits as $fruit) 
+{
+    echo "{$fruit} are {$color}\n";
 }
 
 
 $fruits = array('appple', 'orange', 'bananas');
 
-for ($i = 0; $i < count($fruits); $i++) {
-	echo $fruits[$i] .PHP_EOL; 
+for ($i = 0; $i < count($fruits); $i++) 
+{
+    echo $fruits[$i] .PHP_EOL; 
 }
 
 foreach ($fruits as $fruit) {
-	echo $fruit . PHP_EOL;
-}	
+    echo $fruit . PHP_EOL;
+}   
+
+?>
