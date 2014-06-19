38e8c4f1573ed64b54c3ef0e120bdffcd6cd0202
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-19 17:13:44 -0500

    Updated formatting.

--- for.php
+++ for.php
@@ -2,27 +2,41 @@
 
 fwrite(STDOUT, "Please choose an increment\n");
 
-$increment = trim(fgets(STDIN));
+	$increment = trim(fgets(STDIN));
 
 fwrite(STDOUT, "Please choose a number\n");
 
-$first_num = trim(fgets(STDIN));
+	$first_num = trim(fgets(STDIN));
 
-if (!is_numeric($first_num)) {echo "Please enter a numeric value\n";}
+if (!is_numeric($first_num)) 
+{
+	echo "Please enter a numeric value\n";
+}
 
-if ($first_num = "") {echo "Please enter a numeric value\n";}
+if ($first_num = "") 
+{
+		echo "Please enter a numeric value\n";
+}
 
 fwrite(STDOUT, "Please choose another number\n");
 
 $second_num = trim(fgets(STDIN));
 
-if (!is_numeric($second_num)) {echo "Please enter a numeric value";}	
+if (!is_numeric($second_num)) 
+{
+	echo "Please enter a numeric value";
+}	
 
-if ($second_num = "") {echo "Please enter a numeric value";}	
+if ($second_num = "") 
+{
+	echo "Please enter a numeric value";
+}	
 
-for ($i = $first_num; $i <= $second_num; $i = $increment++) {
+for ($i = $first_num; $i <= $second_num; $i = $increment++) 
+{
 	echo "{$i}\n";
 }
 
+?>
 
 
