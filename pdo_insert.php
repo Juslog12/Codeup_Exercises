7361125317448106629c551edd302b4ab80dc42a
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-20 15:31:57 -0500

    Created connection to SQL using PDO

---
+++ pdo_insert.php
@@ -0,0 +1,14 @@
+<?php
+
+// Get new instance of PDO object
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'justin', 'Maslog12');
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+
+
+
+?>
